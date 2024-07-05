cea2d3f8953157e08297f486e37559843d11e2df
Maxim <maxodegov163@mail.ru>
2024-07-05 09:00:51 +0700

    update test_classes.py

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -29,7 +29,9 @@ def test_category_init(category_pasta, category_gaming_consoles):
 
 @pytest.fixture()
 def product_smartphone():
-    return Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    return Product(
+        "Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+    )
 
 
 @pytest.fixture()
